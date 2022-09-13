d8c69e88013f6f21c92e3d18a77d26ddac87abca
Farhan-hub <farhanuddin.psnl@gmail.com>
2022-09-13 22:51:13 +0600

    added a run date offer at js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// special countdown
+let dayitem = document.querySelector("#days");
+let hoursitem = document.querySelector("#hours");
+let minitem = document.querySelector("#min");
+let secitem = document.querySelector("#sec");
+
+let countdown = () => {
+  let futureDate = new Date("30 october 2022");
+  let currentDate = new Date();
+  let myDate = futureDate - currentDate;
+
+  let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+  let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+  let min = Math.floor(myDate / 1000 / 60) % 60;
+  let sec = Math.floor(myDate / 1000) % 60;
+
+  dayitem.innerHTML = days;
+  hoursitem.innerHTML = hours;
+  minitem.innerHTML = min;
+  secitem.innerHTML = sec;
+};
+
+countdown();
+
+setInterval(countdown, 1000);
